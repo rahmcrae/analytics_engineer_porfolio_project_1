3a28776058e2661310e4ab269553edc8d683ff7d
Rah <rah@consult-rah.com>
2024-11-29 03:29:18 -0500

    dynamically loop through years

--- src/utils/process/data_processor.py
+++ src/utils/process/data_processor.py
@@ -17,7 +17,7 @@ class DataProcessor:
         self.s3_client = self.session.client('s3')
         self.bucket_name = os.getenv("BUCKET_NAME")
         self.api_key = os.getenv("CENSUS_API_KEY")
-        self.year = os.getenv('YEAR')
+        self.year = int(os.getenv('YEAR')) # convert to integer
         self.logger = logging.getLogger(__name__)
         self.base_url = "https://api.census.gov/data/"
 
@@ -43,20 +43,20 @@ class DataProcessor:
 
         return variables 
 
-    def fetch_and_process_data(self, variables_chunk):
+    def fetch_and_process_data(self, year, variables_chunk):
         """Fetches data from the Census API and processes it."""
 
         variables_str = ",".join(variables_chunk) + ",NAME"
 
         # Determine the correct endpoint and geo identifier based on your geo_level logic
         if self.geo_level == "State":
-            base_url = f"{self.base_url}{self.year}/acs/acs1/profile"
+            base_url = f"{self.base_url}{year}/acs/acs1/profile"
             geo_suffix = "state"
         elif self.geo_level == "US":
-            base_url = f"{self.base_url}{self.year}/acs/acs1/profile"
+            base_url = f"{self.base_url}{year}/acs/acs1/profile"
             geo_suffix = "us"
         elif self.geo_level == "Zip":
-            base_url = f"{self.base_url}{self.year}/acs/acs5"
+            base_url = f"{self.base_url}{year}/acs/acs5"
             geo_suffix = "zip%20code%20tabulation%20area"
         else:
             raise ValueError("Unsupported geographical level")
@@ -71,25 +71,30 @@ class DataProcessor:
             return None
         
         return pd.DataFrame(data[1:], columns=data[0])
-
-    def process(self):
-        """Orchestrates data fetching, processing, and saving to S3."""
+    
+    def process_year(self, year):
+        """Processes data for a single year."""
         all_variables = self.get_variables_for_geo()
-
-        # Process data in chunks as needed
         chunk_size = 100
         for i in range(0, len(all_variables), chunk_size):
-            variables_chunk = all_variables[i : i + chunk_size]
-            df = self.fetch_and_process_data(variables_chunk)
+            variables_chunk = all_variables[i:i + chunk_size]
+            df = self.fetch_and_process_data(year, variables_chunk)
             if df is not None:
-                self.save_to_s3(df)
+                self.save_to_s3(year, df)
 
-    def save_to_s3(self, df):
+    def process(self):
+        """Orchestrates data fetching, processing, and saving for all years from start_year to current year."""
+        current_year = datetime.datetime.now().year
+        for year in range(self.year, current_year + 1):
+            self.logger.info(f"Processing data for year: {year}")
+            self.process_year(year)
+
+    def save_to_s3(self, year, df):
         """Saves the processed DataFrame to S3."""
 
         # Save to s3
         timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        s3_path = f"CENSUS_YEAR={self.year}/{self.geo_level}/{timestamp}/{self.geo_level}_data.json"
+        s3_path = f"CENSUS_YEAR={year}/{self.geo_level}/{timestamp}/{self.geo_level}_data.json"
         json_data = df.to_json(orient="records")
         self.s3_client.put_object(Bucket=self.bucket_name, Key=s3_path, Body=json_data)
         self.logger.info(f"Saved {len(df)} rows to S3: {s3_path}")
